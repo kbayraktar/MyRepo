eab719b5fe34534c4165001696ecd0451c9e1c85
Koray Bayraktar <kaybee@web.de>
2016-05-29 12:34:50 +0200

    Log erweitert

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,7 +3,7 @@ public class MyClass {
 
    	public static void main(String[] args) {
 		
-    	// just a comment oder so, und nich anners
+    	// just a comment oder so, und nich anners was
          String a = "abc";
 
 	}
